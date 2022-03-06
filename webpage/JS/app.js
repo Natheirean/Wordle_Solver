dd9b115e3ec4367f5e2fc27eeea3750a125bf1a7
Kai Pham <haipham0115@gmail.com>
2022-03-06 13:40:10 -0800

    add id for input boxes L1 to L5

--- webpage/JS/app.js
+++ webpage/JS/app.js
@@ -1,5 +1,11 @@
 function clear_fields() {
-document.getElementById("wordle-grid").value = "";
-document.getElementById("wordle-grid2").value = "";
-document.getElementById("wordle-grid3").value = "";
+    document.getElementById("wordle-grid").value = "";
+    document.getElementById("wordle-grid2").value = "";
+    document.getElementById("wordle-grid3").value = "";
 }
+
+var inputL1 = document.getElementById('L1-input').value;
+var inputL2 = document.getElementById('L2-input').value;
+var inputL3 = document.getElementById('L3-input').value;
+var inputL4 = document.getElementById('L4-input').value;
+var inputL5 = document.getElementById('L5-input').value;
