737424169b07de20c9e0d5bf36109ddef72d79e2
Christina Laigo <chrhong@hotmail.com>
2022-03-05 22:54:16 -0800

    clear button click

---
+++ webpage/JS/app.js
@@ -0,0 +1,5 @@
+function clear_fields() {
+document.getElementById("wordle-grid").value = "";
+document.getElementById("wordle-grid2").value = "";
+document.getElementById("wordle-grid3").value = "";
+}
